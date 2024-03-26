3e09f6f2b3e5b8276e7555ffdfb25494090a80ea
Anton Kazakevich <71589375+pipistero@users.noreply.github.com>
2024-03-26 19:35:43 +0300

    GridView animation rework

--- Assets/Scripts/Game/Grid/GridViewAnimation.cs
+++ Assets/Scripts/Game/Grid/GridViewAnimation.cs
@@ -7,15 +7,15 @@ namespace Game.Grid
     {
         [Header("Animation settings")] 
         [SerializeField] private Transform _target;
-        [SerializeField] private float _startYPosition;
+        [SerializeField] private float _startScale;
         [SerializeField] private Ease _ease;
         [SerializeField] private float _duration;
         
         public void PlayAppearAnimation()
         {
-            _target.localPosition = Vector3.up * _startYPosition;
+            _target.localScale = Vector3.one * _startScale;
             _target
-                .DOLocalMoveY(0f, _duration)
+                .DOScale(1f, _duration)
                 .SetEase(_ease);
         }
     }
